c4a2ce754b9d62e2c53e17ce7f4f94fa1664ec42
pedroliraa <pedrolirabandeira@gmail.com>
2026-08-27 19:14:36 -0300

    feat: adiciona rota de health check

--- server/src/app.ts
+++ server/src/app.ts
@@ -18,6 +18,12 @@ app.get("/", (_req, res) => {
   });
 });
 
+app.get("/ping", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
+
 app.use("/auth", authRoutes);
 app.use("/events", eventRoutes);
 app.use("/reservations", reservationRoutes);
